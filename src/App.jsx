4fab83b3a1f5ad6e58f40e6b7819064261914c9e
Hendrixx <Cardozo9042@gmail.com>
2025-08-04 14:58:00 +0100

    Updated VUE JS skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,59 @@
-import React, { useState, createContext, useEffect } from 'react'
+import React, { useState, createContext, useEffect } from 'react';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 import { Footer } from './components/Footer';
-import { FaAngleDoubleUp } from "react-icons/fa";
+import { FaAngleDoubleUp } from 'react-icons/fa';
 
 export const myContext = createContext();
 export const App = () => {
-  const [showButton, setShowButton] = useState(false);
-  const [ isDark, setIsDark ] = useState(true);
-  useEffect(() => {
-    const handleScroll = () => {
-      if(window.scrollY > 500){
-        setShowButton(true)
-      } else {
-        setShowButton(false)
-      }
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
-  
-  const handleScrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth'})
-  }
-  return (
-    <div className={`font-Aboreto ${ isDark ? 'bg-gray-950 text-white': 'bg-slate-50 text-black' } flex flex-col`}>
-      <myContext.Provider value={{isDark, setIsDark}}>
-        <Navbar />
-        <main className='px-[1rem] min-lg:px-[5rem] py-[2rem] min-md:px-[2rem] min-xl:px-[12%]'>
-          {showButton && <button className='cursor-pointer p-[0.1rem] bg-white fixed right-[2rem] top-[50%] text-3xl rounded-full' onClick={handleScrollToTop}>
-            <FaAngleDoubleUp  className=' text-sky-400'/>
-          </button>}
-          <Hero />
-          <hr />
-          <About />
-          <hr />
-          <Projects />
-          <hr />
-          <Skills />
-          <hr />
-        </main>
-        <Footer />
-      </myContext.Provider>
-    </div>
-  )
-}
+	const [showButton, setShowButton] = useState(false);
+	const [isDark, setIsDark] = useState(true);
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 500) {
+				setShowButton(true);
+			} else {
+				setShowButton(false);
+			}
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
+	const handleScrollToTop = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	};
+	return (
+		<div
+			className={`font-Aboreto ${
+				isDark ? 'bg-gray-950 text-white' : 'bg-slate-50 text-black'
+			} flex flex-col`}
+		>
+			<myContext.Provider value={{ isDark, setIsDark }}>
+				<Navbar />
+				<main className='px-[1rem] min-lg:px-[5rem] py-[2rem] min-md:px-[2rem] min-xl:px-[12%]'>
+					{showButton && (
+						<button
+							className='cursor-pointer p-[0.1rem] bg-white fixed right-[2rem] top-[50%] text-3xl rounded-full'
+							onClick={handleScrollToTop}
+						>
+							<FaAngleDoubleUp className=' text-sky-400' />
+						</button>
+					)}
+					<Hero />
+					<hr />
+					<About />
+					<hr />
+					<Projects />
+					<hr />
+					<Skills />
+					<hr />
+				</main>
+				<Footer />
+			</myContext.Provider>
+		</div>
+	);
+};
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,65 +1,112 @@
-import React, { useContext } from 'react'
-import { FaGithub } from "react-icons/fa";
-import { CgWebsite } from "react-icons/cg";
+import React, { useContext } from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { CgWebsite } from 'react-icons/cg';
 import { myContext } from '../App';
 
 export const Projects = () => {
-  
-  return (
-    <section id='projects' className='py-[4rem] flex flex-col gap-[2rem]'>
-      <h2 className='text-[2rem] text-blue-400'>Projects</h2>
-      <div className='grid gap-[2rem] min-lg:grid-cols-3 min-[700px]:grid-cols-2 m-auto'>
-      <ProjectsDemo 
-          src='/images/ecommerce_project_image.png'
-          projectName='E-commerce app'
-          description='An online shopping platform that lets you add items to cart without processing payments yet'
-          languages='React, Tailwind, Axios, React Router, API'
-          ghLink='https://github.com/BeeAlmighty/eCommerce-app.git'
-          webLink='https://e-commerce-app-pi-rust.vercel.app/'
-        />
-        <ProjectsDemo 
-          src='/images/weatherApp.png'
-          projectName='Weather App'
-          description='A weather web application that uses an open-source api to get the current web details of any location'
-          languages='React, Tailwind, API, Axios'
-          ghLink='https://github.com/BeeAlmighty/Weather-App-React'
-          webLink='https://weather-app-react-seven-peach.vercel.app/'
-        />
-        <ProjectsDemo 
-          src='/images/quiz-app.png'
-          projectName='Quiz App'
-          description='A quiz app that tests your knowledge in different sports'
-          languages='React, Tailwind, React Router'
-          ghLink='https://github.com/BeeAlmighty/Sports-Quiz-App'
-          webLink='https://sports-quiz-app.vercel.app/'
-        />
-        <ProjectsDemo 
-          src='/images/tip-calculator.png'
-          projectName='Tip Calculator'
-          description='A basic app that calculates how the bill and tip is shared amongst people'
-          languages='HTML, CSS, JAVASCRIPT'
-          ghLink='https://github.com/BeeAlmighty/Tip-calculator-App'
-          webLink='https://tip-calculator-app-git-main-beealmightys-projects.vercel.app/'
-        />
-      </div>
-    </section>
-  )
-}
+	return (
+		<section
+			id='projects'
+			className='py-[4rem] flex flex-col gap-[2rem]'
+		>
+			<h2 className='text-[2rem] text-blue-400'>Projects</h2>
+			<div className='grid gap-[2rem] min-lg:grid-cols-3 min-[700px]:grid-cols-2 m-auto'>
+				<ProjectsDemo
+					src='/images/ecommerce_project_image.png'
+					projectName='E-commerce app'
+					description='An online shopping platform that lets you add items to cart without processing payments yet'
+					languages='React, Tailwind, Axios, React Router, API'
+					ghLink='https://github.com/BeeAlmighty/eCommerce-app.git'
+					webLink='https://e-commerce-app-pi-rust.vercel.app/'
+				/>
+				<ProjectsDemo
+					src='/images/weatherApp.png'
+					projectName='Weather App'
+					description='A weather web application that uses an open-source api to get the current web details of any location'
+					languages='React, Tailwind, API, Axios'
+					ghLink='https://github.com/BeeAlmighty/Weather-App-React'
+					webLink='https://weather-app-react-seven-peach.vercel.app/'
+				/>
+				<ProjectsDemo
+					src='/images/quiz-app.png'
+					projectName='Quiz App'
+					description='A quiz app that tests your knowledge in different sports'
+					languages='React, Tailwind, React Router'
+					ghLink='https://github.com/BeeAlmighty/Sports-Quiz-App'
+					webLink='https://sports-quiz-app.vercel.app/'
+				/>
+				<ProjectsDemo
+					src='/images/tip-calculator.png'
+					projectName='Tip Calculator'
+					description='A basic app that calculates how the bill and tip is shared amongst people'
+					languages='HTML, CSS, JAVASCRIPT'
+					ghLink='https://github.com/BeeAlmighty/Tip-calculator-App'
+					webLink='https://tip-calculator-app-git-main-beealmightys-projects.vercel.app/'
+				/>
+				<ProjectsDemo
+					src='/images/agricon.jpeg'
+					projectName='AGRICON'
+					description='an innovative platform purposed to transform agriculture by connecting farmers to nearby storage facilities. Our mission is to reduce post-harvest losses and boost efficiency across the agricultural value chain.'
+					languages='REACT, TAILWIND, EXPRESS, MONGODB'
+					ghLink='https://github.com/InternPulse/agricon-frontend.git'
+					webLink='https://agricon.com.ng/'
+				/>
+			</div>
+		</section>
+	);
+};
 
-const ProjectsDemo = ({ src, projectName, description, languages, ghLink, webLink }) => {
-  const {isDark} = useContext(myContext)
-  return (
-    <div className={`${isDark ? 'text-black bg-blue-100 hover:shadow-gray-400' : 'text-white bg-gray-600 hover:shadow-gray-500'} grid rounded-lg cursor-pointer shadow-md duration-500 max-w-[22rem]`}>
-      <img src={src} alt={src} className='rounded-t-lg' />
-      <div className='p-[1rem] flex flex-col gap-[1rem]'>
-        <h2 className='text-2xl'>{projectName}</h2>
-        <p>{description}</p>
-        <p className='font-CormorantU uppercase font-extrabold'>{languages}</p>
-        <div className='flex gap-2.5'>
-          <a href={ghLink} target='_blank' className={`${isDark ? 'hover:text-black text-gray-500' : 'hover:text-white text-gray-400'} duration-200 ease-in`}><FaGithub className='text-[2rem] cursor-pointer'/></a>
-          <a href={webLink} target='_blank' className={`${isDark ? 'hover:text-black text-gray-500' : 'hover:text-white text-gray-400'} duration-200 ease-in`}><CgWebsite className='text-[2rem] cursor-pointer'/></a>
-        </div>
-      </div>
-    </div>
-  )
-}
+const ProjectsDemo = ({
+	src,
+	projectName,
+	description,
+	languages,
+	ghLink,
+	webLink,
+}) => {
+	const { isDark } = useContext(myContext);
+	return (
+		<div
+			className={`${
+				isDark
+					? 'text-black bg-blue-100 hover:shadow-gray-400'
+					: 'text-white bg-gray-600 hover:shadow-gray-500'
+			} grid rounded-lg cursor-pointer shadow-md duration-500 max-w-[22rem]`}
+		>
+			<img
+				src={src}
+				alt={src}
+				className='rounded-t-lg'
+			/>
+			<div className='p-[1rem] flex flex-col gap-[1rem]'>
+				<h2 className='text-2xl'>{projectName}</h2>
+				<p>{description}</p>
+				<p className='font-CormorantU uppercase font-extrabold'>{languages}</p>
+				<div className='flex gap-2.5'>
+					<a
+						href={ghLink}
+						target='_blank'
+						className={`${
+							isDark
+								? 'hover:text-black text-gray-500'
+								: 'hover:text-white text-gray-400'
+						} duration-200 ease-in`}
+					>
+						<FaGithub className='text-[2rem] cursor-pointer' />
+					</a>
+					<a
+						href={webLink}
+						target='_blank'
+						className={`${
+							isDark
+								? 'hover:text-black text-gray-500'
+								: 'hover:text-white text-gray-400'
+						} duration-200 ease-in`}
+					>
+						<CgWebsite className='text-[2rem] cursor-pointer' />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
